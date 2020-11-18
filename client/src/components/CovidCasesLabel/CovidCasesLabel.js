e79958579330beca7c19fa46b690a91a0b9975e0
Pat Rivet <patrickmrivet@gmail.com>
2020-11-18 18:23:18 +0000

    Refactor: left align country card data

--- client/src/components/CovidCasesLabel/CovidCasesLabel.js
+++ client/src/components/CovidCasesLabel/CovidCasesLabel.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './CovidCasesLabel.css';
 
-const CovidCasesLabel = () => (<div className="casesLabel">Cases</div>)
+const CovidCasesLabel = () => (<div className="casesLabel">New Cases</div>)
 
 export default CovidCasesLabel;
--- client/src/components/CovidDeathsLabel/CovidDeathsLabel.js
+++ client/src/components/CovidDeathsLabel/CovidDeathsLabel.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './CovidDeathsLabel.css';
 
-const CovidDeathsLabel = () => (<div className="deathsLabel">Deaths</div>)
+const CovidDeathsLabel = () => (<div className="deathsLabel">New Deaths</div>)
 
 export default CovidDeathsLabel;
