2216fca64448746e7a043693ada4688c79cccbd3
Pat Rivet <patrickmrivet@gmail.com>
2020-12-16 16:05:23 +0000

    Refactor: - Set space-evenly justify content for country cards. - Add fav and expand icons to country card header.

--- client/src/components/CountryHeader/CountryHeader.js
+++ client/src/components/CountryHeader/CountryHeader.js
@@ -1,26 +1,48 @@
 import React from 'react';
 import './CountryHeader.css';
 
+// Images
+import FavouriteAdd from '../../assets/imgs/favourite_add.png';
+import FavouriteRemove from '../../assets/imgs/favourite_remove.png';
+import ExpandArrow from '../../assets/imgs/expand_arrow.png';
+
 const PUBLIC_URL = process.env.PUBLIC_URL;
+const iconStyle = { width: '16px', height: '16px' };
 
 const CountryHeader = ({ name, countryCode }) => {
   return (
     <div className='header'>
-      <img
-        className='header__img'
-        src={`
-          ${PUBLIC_URL}
-        /imgs/flags/${countryCode.toLowerCase()}.svg`}
-        alt=''
-      ></img>
-      {/* If country name > 17 in length -add class to use smaller font. */}
-      <p
-        className={`header__name ${
-          name.length > 17 ? 'header--smallFont' : ''
-        }`}
-      >
-        {name}
-      </p>
+      <div className='header__id'>
+        <img
+          className='header__img'
+          src={`
+            ${PUBLIC_URL}
+          /imgs/flags/${countryCode.toLowerCase()}.svg`}
+          alt=''
+        ></img>
+        {/* If country name > 17 in length -add class to use smaller font. */}
+        <p
+          className={`header__name ${
+            name.length > 17 ? 'header--smallFont' : ''
+          }`}
+        >
+          {name}
+        </p>
+      </div>
+      <div className='header__icons'>
+        <img
+          className='header__expandIcon'
+          src={ExpandArrow}
+          alt='Expand_CountryCard'
+          style={iconStyle}
+        />
+        <img
+          className='header__favouriteIcon'
+          src={FavouriteAdd}
+          alt='Favourite_Add'
+          style={iconStyle}
+        />
+      </div>
     </div>
   );
 };
