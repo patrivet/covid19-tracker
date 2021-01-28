b0484da8383e66b019c387db5848ecfb90059811
Pat Rivet <patrickmrivet@gmail.com>
2021-01-28 23:28:31 +0000

    Refactor: Add hover and link to country drill on all of country card. - Tidy margin on country drill view.

--- client/src/components/CountryCard/CountryCard.js
+++ client/src/components/CountryCard/CountryCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './CountryCard.css';
 
 // Custom components
@@ -11,28 +12,48 @@ import CasesLabel from '../CasesLabel';
 import DeathsLabel from '../DeathsLabel';
 import CasesYesterday from '../CasesYesterday';
 import DeathsYesterday from '../DeathsYesterday';
+import { setSelectedCountry } from '../../actions/actions';
+import store from '../../store';
 
 const CountryCard = ({ country }) => {
+  const linkContent = {
+    pathname: `country/${country.ISO2}`,
+    // pass the country as state for the link
+    state: country,
+  };
+
+  const handleSetCountry = () => {
+    store.dispatch(
+      setSelectedCountry({
+        name: country.name,
+        countryCode: country.ISO2,
+      })
+    );
+  };
+
   return (
     <div className='CountryCard'>
       <CountryHeader country={country} />
-
-      <div className='CountryCard__statsContainer'>
-        <CasesLabel />
-        <DeathsLabel />
-        <CasesToday
-          todayCases={country.todayData.todayCases}
-          yesterdayCases={country.yesterdayData.todayCases}
-        />
-        <DeathsToday
-          todayDeaths={country.todayData.todayDeaths}
-          yesterdayDeaths={country.yesterdayData.todayDeaths}
-        />
-        <DeathsTotal totalDeaths={country.todayData.deaths} />
-        <CasesYesterday yesterdayCases={country.yesterdayData.todayCases} />
-        <DeathsYesterday yesterdayDeaths={country.yesterdayData.todayDeaths} />
-        <CasesTotal totalCases={country.todayData.cases} />
-      </div>
+      <Link to={linkContent} onClick={handleSetCountry}>
+        <div className='CountryCard__statsContainer'>
+          <CasesLabel />
+          <DeathsLabel />
+          <CasesToday
+            todayCases={country.todayData.todayCases}
+            yesterdayCases={country.yesterdayData.todayCases}
+          />
+          <DeathsToday
+            todayDeaths={country.todayData.todayDeaths}
+            yesterdayDeaths={country.yesterdayData.todayDeaths}
+          />
+          <DeathsTotal totalDeaths={country.todayData.deaths} />
+          <CasesYesterday yesterdayCases={country.yesterdayData.todayCases} />
+          <DeathsYesterday
+            yesterdayDeaths={country.yesterdayData.todayDeaths}
+          />
+          <CasesTotal totalCases={country.todayData.cases} />
+        </div>
+      </Link>
     </div>
   );
 };
