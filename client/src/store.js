8ee60e5070288e5c93759064f131a73edc9fb18a
Pat Rivet <patrickmrivet@gmail.com>
2021-02-06 22:17:41 +0000

    Refactor: Only show redux logger when dev environment set in .env file.

--- client/src/store.js
+++ client/src/store.js
@@ -1,8 +1,11 @@
+import config from './config';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from './reducers/reducer';
 import logger from 'redux-logger';
 
-const store = createStore(reducer, compose(applyMiddleware(thunk, logger)));
-
+const store =
+  config.ENVIRONMENT === 'dev'
+    ? createStore(reducer, compose(applyMiddleware(thunk, logger)))
+    : createStore(reducer, compose(applyMiddleware(thunk)));
 export default store;
