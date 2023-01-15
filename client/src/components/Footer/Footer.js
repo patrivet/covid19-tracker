581188f5f72fc70a5d57d3d83398216c9de37e36
Pat Rivet <patrickmrivet@gmail.com>
2023-01-15 15:11:28 +0000

    Remove unused hook.

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import store from '../../store';
 import { setDarkMode } from '../../actions/actions';
